ce4dcc1a5fcfac324fd6e8772134b6c2c239b1b7
hmlewis-astro <21116401+hmlewis-astro@users.noreply.github.com>
2021-10-14 17:49:20 -0400

    add qr code and final code files

--- dogrates_pipeline.sh
+++ dogrates_pipeline.sh
@@ -8,12 +8,16 @@ conda activate metis
 echo "Getting new Tweets..."
 python twitter_api.py
 
-mongod --dbpath ~/data/db
+mongod --dbpath ~/data/db --quiet
 
 echo "Updating MongoDB..."
 python create_mongo_db.py
 
 echo "Getting model predictions..."
-python final_model_prediction.py
+python final_model_predictions.py
+
+echo "Sending new Tweets to Atlas database to be served to Heroku..."
+mongoimport --uri "mongodb+srv://<username>:<password>@engineering-cluster.mh4jb.mongodb.net/dog_rates_images?retryWrites=true&w=majority" --collection tweets --drop --type json --jsonArray --file dog_rates_tweets.json
+mongoimport --uri "mongodb+srv://<username>:<password>@engineering-cluster.mh4jb.mongodb.net/dog_rates_images?retryWrites=true&w=majority" --collection media --drop --type json --jsonArray --file dog_rates_tweets_media.json
 
 echo "Done!"
--- final_model_predictions.py
+++ final_model_predictions.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import re
 import requests
 import csv
+import json
 
 import pandas as pd
 import numpy as np
@@ -90,11 +91,9 @@ get_all_pred = False
 
 # Get model predictions for Tweeted media
 for tweet in tqdm(tweets.find(), total=tweets.estimated_document_count()):
-
     # Do not get model predictions if predictions already exist
-    if not get_all_pred:
-        if 'predicted_breed' in tweet.keys():
-            continue
+    if not get_all_pred and 'predicted_breed' in tweet.keys():
+        continue
 
     med =  tweets_media.find({'tweet_id':tweet['id']}, )
     med = list(med)[0]
@@ -105,14 +104,54 @@ for tweet in tqdm(tweets.find(), total=tweets.estimated_document_count()):
             med_top_breeds = breed_pred(med_url)
             predicted_breeds.append(med_top_breeds.numpy())
 
-            med_top_components = tf.reshape(tf.math.top_k(med_top_breeds, k=3).indices,shape=[-1])
-            med_top_breeds = [breed_name(i) for i in med_top_components]
-            med_top_breeds = list(map(split_breed_name, med_top_breeds))
-            tweets_media.update_one({'tweet_id':tweet['id']}, {"$set": {"predicted_breed": med_top_breeds}})
+            med_top_idx = tf.reshape(tf.math.top_k(med_top_breeds, k=3).indices,shape=[-1])
+            med_top_probs = [str(med_top_breeds.numpy()[0][i]) for i in list(med_top_idx.numpy())]
+            med_top_breed_names = [breed_name(i) for i in med_top_idx]
+            med_top_breed_names = list(map(split_breed_name, med_top_breed_names))
+
+            tweets_media.update_one({'tweet_id':tweet['id']}, {"$set": {f"{key}.predicted_breed": {'breed':med_top_breed_names, 'probability':med_top_probs}}})
 
     predicted_breeds = sum(predicted_breeds)
 
-    top_components = tf.reshape(tf.math.top_k(predicted_breeds, k=3).indices,shape=[-1])
-    top_breeds = [breed_name(i) for i in top_components]
-    top_breeds = list(map(split_breed_name, top_breeds))
-    tweets.update_one({'id': tweet['id']}, {"$set": {"predicted_breed": top_breeds}})
+    top_idx = tf.reshape(tf.math.top_k(predicted_breeds, k=3).indices,shape=[-1])
+    top_probs = [str(predicted_breeds[0][i]) for i in list(top_idx.numpy())]
+    top_breed_names = [breed_name(i) for i in top_idx]
+    top_breed_names = list(map(split_breed_name, top_breed_names))
+
+    tweets.update_one({'id': tweet['id']}, {"$set": {f"predicted_breed": {'breed':top_breed_names, 'probability':top_probs}}})
+
+# for tweet in tqdm(tweets.find(), total=tweets.estimated_document_count()):
+#     print(tweet['predicted_breed'])
+#
+#     med =  tweets_media.find({'tweet_id':tweet['id']}, )
+#     med = list(med)[0]
+#     for key in med.keys():
+#         print(key)
+#         if re.search('media', key):
+#             print(med[key])
+#
+#     break
+
+# tweets_list = list(tweets.find({'predicted_breed.breed': {'$all': ['American Staffordshire Terrier']}}))
+# print(len(tweets_list))
+
+
+cursor = tweets.find({})
+doc_count = tweets.count_documents({}) - 1
+with open('dog_rates_tweets.json', 'w') as file:
+    file.write('[')
+    for i,document in enumerate(cursor):
+        file.write(json.dumps(document, default=str))
+        if i != doc_count:
+            file.write(',')
+    file.write(']')
+
+cursor = tweets_media.find({})
+doc_count = tweets_media.count_documents({}) - 1
+with open('dog_rates_tweets_media.json', 'w') as file:
+    file.write('[')
+    for i,document in enumerate(cursor):
+        file.write(json.dumps(document, default=str))
+        if i != doc_count:
+            file.write(',')
+    file.write(']')
